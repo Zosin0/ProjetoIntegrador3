6be2c595a998089c291129391b6d8b9712f66d5e
Lucas Zoser <53053622+Zosin0@users.noreply.github.com>
2024-03-05 19:32:41 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Fast Ticket</Text>
+      <Button 
+        title="Clique aqui"
+        onPress={() => alert('Botão pressionado!')}
+      />
       <StatusBar style="auto" />
     </View>
-  );
+  );  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ README.md
@@ -0,0 +1,1 @@
+## React Native App para revolucionar o pagamento de tickets

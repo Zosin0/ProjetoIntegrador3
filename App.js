dde1fe74a9616187ba0902748d0ce5a34d1dbb66
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2024-04-02 20:21:09 -0300

    sem o header

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ const Stack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
